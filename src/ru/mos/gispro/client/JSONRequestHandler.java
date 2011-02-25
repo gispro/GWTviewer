b3dc67a806fe9d7a0a8b60a97617871612108a66
ekoklin <ekoklin@f11919cc-cac4-be47-8c43-ed2f22550f7b>
2011-02-25 16:03:45 +0000

    git-svn-id: http://lightroom.gispro.local:8443/svn/GWTviewer@14 f11919cc-cac4-be47-8c43-ed2f22550f7b

---
+++ src/ru/mos/gispro/client/JSONRequestHandler.java
@@ -0,0 +1,7 @@
+package ru.mos.gispro.client;
+
+import com.google.gwt.core.client.JavaScriptObject;
+
+public interface JSONRequestHandler {
+	public void onRequestComplete(JavaScriptObject json);
+}
