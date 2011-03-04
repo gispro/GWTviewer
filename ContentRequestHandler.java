c73b13d69841fa5ce4f1e39fc6f56b6cb2a44ef4
ekoklin <ekoklin@f11919cc-cac4-be47-8c43-ed2f22550f7b>
2011-03-04 10:26:12 +0000

    git-svn-id: http://lightroom.gispro.local:8443/svn/GWTviewer@38 f11919cc-cac4-be47-8c43-ed2f22550f7b

--- ContentRequestHandler.java
+++
@@ -1,8 +0,0 @@
-package ru.mos.gispro.client;
-
-import com.google.gwt.xml.client.Document;
-
-public interface ContentRequestHandler
-{
-    public void onRequestComplete(com.google.gwt.xml.client.Document content);
-}
