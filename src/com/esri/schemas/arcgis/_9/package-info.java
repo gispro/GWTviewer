4ae83cc5161a01061ba86c22f07bee41637f52a5
ekoklin <ekoklin@f11919cc-cac4-be47-8c43-ed2f22550f7b>
2011-07-07 13:23:20 +0000

    git-svn-id: http://lightroom.gispro.local:8443/svn/GWTviewer@104 f11919cc-cac4-be47-8c43-ed2f22550f7b

--- src/com/esri/schemas/arcgis/_9/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.esri.com/schemas/ArcGIS/9.3")
-package com.esri.schemas.arcgis._9;
--- src/ru/mos/gispro/client/IBaseMap.java
+++
@@ -1,8 +0,0 @@
-package ru.mos.gispro.client;
-
-public interface IBaseMap
-{
-    public void setBaseMap         (String baseMapName);
-    public void closeBaseMapWindow (                  );
-    public void removeBaseMap      (                  );
-}
